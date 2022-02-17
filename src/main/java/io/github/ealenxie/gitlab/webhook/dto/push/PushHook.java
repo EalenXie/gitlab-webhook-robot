847e3c27b0fef59b778088d74c3ab9a8a39e4723
EalenXie <ealenxie@qq.com>
2022-02-17 15:59:09 +0800

    fix: 获取username

--- src/main/java/io/github/ealenxie/gitlab/webhook/dto/push/PushHook.java
+++ src/main/java/io/github/ealenxie/gitlab/webhook/dto/push/PushHook.java
@@ -55,7 +55,7 @@ public class PushHook implements MarkDownMsg {
         String branch = refSplit[refSplit.length - 1];
         sb.append(String.format("[[%s:%s]](%s/-/tree/%s) ", project.getName(), branch, project.getWebUrl(), branch));
         String c = commits.size() > 1 ? "commits" : "commit";
-        String user = String.format("[%s](%s)", userUsername, UserUtils.getUserHomePage(project.getWebUrl(), userUsername));
+        String user = userUsername == null ? userName : String.format("[%s](%s)", userUsername, UserUtils.getUserHomePage(project.getWebUrl(), userUsername));
         sb.append(String.format("<font color='#000000'>%s %s new %s by %s %s </font>%n%n", eventName, totalCommitsCount, c, new Emoji("\uD83D\uDE00"), user));
         for (Commit vo : commits) {
             sb.append(String.format("- [%s](%s) %s - %s%n", vo.getId().substring(0, 8), vo.getUrl(), vo.getAuthor().getName(), vo.getTitle()));
